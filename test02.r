bcb0eb54b32d8e530c892dfb13cef1fc0cb39456
Josep Vela <josep.vela.albesa@gmail.com>
2014-06-09 11:30:17 +0200

    Test magit

---
+++ test02.r
@@ -0,0 +1,12 @@
+## ----------------------------------------------------------------------
+## Author   : Josep Vela - <jv@actioadfuturum.com>
+## Subject  : Test R from Emacs 2
+## Revision : $Id: test02.r, v 1.0 2014/06/09 11:18:S jva $
+## ----------------------------------------------------------------------
+
+head(pressure)
+hist(pressure$pressure)
+
+## GIT
+# magit-status
+
